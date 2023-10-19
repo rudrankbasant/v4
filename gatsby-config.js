c7b5b24b243438a653b5a6bc3bc9184a277906da
rudrankbasant <rudrank123@gmail.com>
2023-10-19 12:03:29 +0530

    feat: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Rudrank Basant',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'I build apps and backend.',
+    siteUrl: 'https://rudrank.tech', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@rudrankbasant',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'RudrankBasant',
+        short_name: 'Rudrank Basant',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
